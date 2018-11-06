193143b32cb5ce5a2f87227d3cce59e24a4847f4
Ahmed Marzook <m4marzook@gmail.com>
2018-11-06 18:32:04 +0000

    Thermostat goes up and down

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -11,4 +11,14 @@ describe('', function() {
   it('starts at 20 degrees', function() {
     expect(thermostat.temperature).toEqual(20);
   });
+
+  it('increases temperature with up()', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('Decreases temperature with down()', function() {
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
 });
