971b9ba52291899fe820a033abffe0bdd923afa7
Ahmed Marzook <m4marzook@gmail.com>
2018-11-06 18:08:07 +0000

    Starting 20 Degrees passing

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('', function() {
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees', function() {
+    expect(thermostat.temperature).toEqual(20);
+  });
+});
