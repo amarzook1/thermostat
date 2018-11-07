fac7fe2c093ce67f8cae4c2361937fb7a18f449d
Ahmed Marzook <m4marzook@gmail.com>
2018-11-07 10:01:13 +0000

    Power saving mode code and test passed

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -21,4 +21,15 @@ describe('', function() {
     thermostat.down();
     expect(thermostat.getCurrentTemperature()).toEqual(19);
   });
+
+  it('has a minimum of 10 degrees', function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
+  it('has a power saving mode on by default', function() {
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  })
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,11 +1,21 @@
 'use strict';
 
 function Thermostat() {
+  this.MINIMUM_TEMPERATURE = 10;
   this.temperature = 20;
+  this.powerSavingMode = true;
 }
 
 Thermostat.prototype.getCurrentTemperature = function() {
   return this.temperature;
+};
+
+Thermostat.prototype.isMinimumTemperature = function () {
+  return this.temperature === this.MINIMUM_TEMPERATURE;
+};
+
+Thermostat.prototype.isPowerSavingModeOn = function () {
+  return this.powerSavingMode === true;
 };
 
 Thermostat.prototype.up = function() {
@@ -13,5 +23,8 @@ Thermostat.prototype.up = function() {
 };
 
 Thermostat.prototype.down = function () {
+  if (this.isMinimumTemperature()) {
+    return;
+  }
   this.temperature -= 1;
 };
